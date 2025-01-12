df6fcef7138ef2bcd42600f0bc75ec1d57f38e52
sajitha00 <sajithageevinda.sd@gmail.com>
2025-01-13 04:21:25 +0530

    code refactor

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -33,7 +33,7 @@ const RelatedDoctors = ({ speciality, docId }) => {
                     </div>
                 ))}
             </div>
-            {/* <button className='bg-[#EAEFFF] text-gray-600 px-12 py-3 rounded-full mt-10'>more</button> */}
+            
         </div>
     )
 }
