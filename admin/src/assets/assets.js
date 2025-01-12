b0df36a76c299d54421ea9e80744fd6f5a02c2bc
binathlaknaka <kablaknaka@students.nsbm.ac.lk>
2025-01-13 02:55:08 +0530

    admin test

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,43 @@
+import add_icon from './add_icon.svg'
+import admin_logo from './admin_logo.svg'
+import InfiniteCare from './InfiniteCare.svg'
+import appointmentDA from './appointmentDA.svg'
+import cancel_icon from './cancel_icon.svg'
+import doctor_icon from './doctor_icon.svg'
+import home from './home.svg'
+import usersD from './usersD.svg'
+import addDoctor from './addDoctor.svg'
+import upload_area from './upload_area.svg'
+import list_icon from './list_icon.svg'
+import tick_icon from './tick_icon.svg'
+import appointments_icon from './appointments_icon.svg'
+import earning_icon from './earning_icon.svg'
+import patients_icon from './patients_icon.svg'
+import Appointment from './Appointment.svg'
+import Doctor from './Doctor.svg'
+import User from './User.svg'
+import Booking from './Booking.svg'
+import Earning from './Earning.svg'
+
+export const assets = {
+    add_icon,
+    admin_logo,
+    InfiniteCare,
+    appointmentDA,
+    cancel_icon,
+    doctor_icon,
+    upload_area,
+    home,
+    addDoctor,
+    patients_icon,
+    usersD,
+    list_icon,
+    tick_icon,
+    appointments_icon,
+    earning_icon,
+    Appointment,
+    Doctor,
+    User,
+    Booking,
+    Earning
+}
---
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -0,0 +1,67 @@
+import React, { useContext, useEffect } from 'react'
+import { assets } from '../../assets/assets'
+import { AdminContext } from '../../context/AdminContext'
+import { AppContext } from '../../context/AppContext'
+
+const Dashboard = () => {
+
+  const { aToken, getDashData, cancelAppointment, dashData } = useContext(AdminContext)
+  const { slotDateFormat } = useContext(AppContext)
+
+  useEffect(() => {
+    if (aToken) {
+      getDashData()
+    }
+  }, [aToken])
+
+  return dashData && (
+    <div className='m-8'>
+
+      <div className='flex flex-wrap gap-7'>
+        <div className='flex items-center gap-2 bg-white hover:bg-second p-4 min-w-52 rounded-xl shadow-sm border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all py-5'>
+          <img className='w-14' src={assets.Doctor} alt="" />
+          <div>
+            <p className='text-xl font-semibold text-gray-600'>{dashData.doctors}</p>
+            <p className='text-gray-400'>Doctors</p>
+          </div>
+        </div>
+        <div className='flex items-center gap-2 bg-white hover:bg-second p-4 min-w-52 rounded-xl shadow-sm border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all py-5'>
+          <img className='w-14' src={assets.Appointment} alt="" />
+          <div>
+            <p className='text-xl font-semibold text-gray-600'>{dashData.appointments}</p>
+            <p className='text-gray-400'>Appointments</p>
+          </div>
+        </div>
+        <div className='flex items-center gap-2 bg-white hover:bg-second p-4 min-w-52 rounded-xl shadow-sm border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all py-5'>
+          <img className='w-14' src={assets.User} alt="" />
+          <div>
+            <p className='text-xl font-semibold text-gray-600'>{dashData.patients}</p>
+            <p className='text-gray-400'>Patients</p></div>
+        </div>
+      </div>
+
+      <div className='bg-white'>
+        <div className='flex items-center gap-2.5 px-4 py-4 mt-10 rounded-t-lg border shadow-sm'>
+          <img src={assets.Booking} alt="" />
+          <p className='font-semibold'>Latest Bookings</p>
+        </div>
+
+        <div className='pt-4 border border-t-0'>
+          {dashData.latestAppointments.slice(0, 5).map((item, index) => (
+            <div className='flex items-center px-6 py-3 gap-3 hover:bg-gray-100' key={index}>
+              <img className='rounded-full w-10' src={item.docData.image} alt="" />
+              <div className='flex-1 text-sm'>
+                <p className='text-gray-800 font-medium'>{item.docData.name}</p>
+                <p className='text-gray-600 '>Booking on {slotDateFormat(item.slotDate)}</p>
+              </div>
+              {item.cancelled ? <p className='text-red-400 text-xs font-medium'>Cancelled</p> : item.isCompleted ? <p className='text-green-500 text-xs font-medium'>Completed</p> : <img onClick={() => cancelAppointment(item._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />}
+            </div>
+          ))}
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Dashboard
