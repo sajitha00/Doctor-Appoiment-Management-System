5761b044920d73fa45f7ce25e4b3cf3905e3b657
Maneesha Kaushalya <148955760+maneesha2002@users.noreply.github.com>
2025-01-13 06:15:59 +0530

    multer.js

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,5 +1,6 @@
 import multer from "multer";
 
+// Configuring the storage settings for multer
 const storage = multer.diskStorage({
     filename: function (req, file, callback) {
         callback(null, file.originalname)
