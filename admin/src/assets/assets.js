4801af28b8d4acaf71d52db063eaae9b30f8daa7
binathlaknaka <kablaknaka@students.nsbm.ac.lk>
2025-01-13 10:02:31 +0530

    admin assets managing 2

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,14 +1,12 @@
 import InfiniteCare from './InfiniteCare.svg'
 import appointmentDA from './appointmentDA.svg'
 import cancel_icon from './cancel_icon.svg'
-import doctor_icon from './doctor_icon.svg'
 import home from './home.svg'
 import usersD from './usersD.svg'
 import addDoctor from './addDoctor.svg'
 import upload_area from './upload_area.svg'
 import list_icon from './list_icon.svg'
 import tick_icon from './tick_icon.svg'
-import appointments_icon from './appointments_icon.svg'
 import earning_icon from './earning_icon.svg'
 import patients_icon from './patients_icon.svg'
 import Appointment from './Appointment.svg'
@@ -21,7 +19,6 @@ export const assets = {
     InfiniteCare,
     appointmentDA,
     cancel_icon,
-    doctor_icon,
     upload_area,
     home,
     addDoctor,
@@ -29,7 +26,6 @@ export const assets = {
     usersD,
     list_icon,
     tick_icon,
-    appointments_icon,
     earning_icon,
     Appointment,
     Doctor,
